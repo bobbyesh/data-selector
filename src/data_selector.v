985974fbc26b832ed78447996fb986efcb3a360f
Bobby Eshleman <eshleman@pdx.edu>
2018-05-08 20:30:30 -0700

    Init commit

---
+++ src/data_selector.v
@@ -0,0 +1,29 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: None
+// Engineer:  Bobby Eshleman
+// 
+// Create Date:    20:22:00 05/08/2018 
+// Design Name: 
+// Module Name:    data_selector 
+// Project Name: 
+// Target Devices: 
+// Tool versions: 
+// Description: 
+//
+// Dependencies: 
+//
+// Revision: 
+// Revision 0.01 - File Created
+// Additional Comments: 
+//
+//////////////////////////////////////////////////////////////////////////////////
+module data_selector(
+    input A,
+    input B,
+    input SEL,
+    output Q
+    );
+
+
+endmodule
